1cdbac32426afb553f14fcd4f41ecda0cd3c2be8
maxpaul04 <maximilian.paul04@gmail.com>
2026-07-21 12:25:22 -0300

    add basic input form for testing

--- lib/screens/add_city_page.dart
+++ lib/screens/add_city_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AddCityPage extends StatelessWidget {
-  const AddCityPage ({super.key});
+  const AddCityPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +9,56 @@ class AddCityPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Add City'),
       ),
-      body: const Center(
-        child: Text('here you will be able to add and document new cities and trips'),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Add a city you have visited', style: Theme.of(context).textTheme.headlineMedium),
+            const SizedBox(height: 16),
+
+            //Input Form for the city entry
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'City Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            //Input form for the country
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Country',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            //Input form for the review comment
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Review Comment',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            //TODO Input form for the rating --> only accept from 1-5 in 0.5 increments
+
+            //Submit Button
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  print('Button pressed (debug)');
+                },
+                child: const Text('Save Journey'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
