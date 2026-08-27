1a77badac9d327931f79cb128f9da1bbbeceb132
maxpaul04 <maximilian.paul04@gmail.com>
2026-08-27 14:18:50 +0200

    start implementation to add lookup for the continent through static json file (API does not provide the continent

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'main_navigation.dart';
 import 'themes/app_theme.dart';
+import 'utils/continent_mapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
+  // Load the country-to-continent mapping for autofilling city data
+  await ContinentMapper.loadMapping();
+  
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
